10531fcc7debaee26de3508941a44398ef9b5dda
David-Rambolajaona <124687880+David-Rambolajaona@users.noreply.github.com>
2024-06-02 20:38:24 +0300

    Update accept-licenses.sh

--- accept-licenses.sh
+++ accept-licenses.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Emplacement de l'outil sdkmanager
-SDKMANAGER="${ANDROID_SDK_ROOT}/cmdline-tools/latest/bin/sdkmanager"
+SDKMANAGER="${ANDROID_SDK_ROOT}/cmdline-tools/bin/sdkmanager"
 
 # Accepter les licences
 yes | $SDKMANAGER --licenses
