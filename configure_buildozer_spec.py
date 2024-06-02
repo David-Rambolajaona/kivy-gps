064e8cc44b17467d5055c891940881e7c8398dcc
David-Rambolajaona <124687880+David-Rambolajaona@users.noreply.github.com>
2024-06-02 17:17:06 +0300

    Update configure_buildozer_spec.py

--- configure_buildozer_spec.py
+++ configure_buildozer_spec.py
@@ -1,11 +1,16 @@
 # configure_buildozer_spec.py
 import configparser
+import sys
+
+# Récupérer les arguments de ligne de commande
+sdk_path = sys.argv[1]
+ndk_path = sys.argv[2]
 
 config = configparser.ConfigParser()
 config.read('buildozer.spec')
 
-config.set('app', 'android.sdk_path', '${{ github.workspace }}/android-sdk')
-config.set('app', 'android.ndk_path', '${{ github.workspace }}/android-ndk')
+config.set('app', 'android.sdk_path', sdk_path)
+config.set('app', 'android.ndk_path', ndk_path)
 
 with open('buildozer.spec', 'w') as configfile:
     config.write(configfile)
